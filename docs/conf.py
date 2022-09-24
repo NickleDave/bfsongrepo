018081bccbe28ed1bfd2f6de0532912d0f8481e2
David Nicholson <nickledave@users.noreply.github.com>
2022-09-24 19:57:31 -0400

    Move docs/source/conf.py -> docs, change config

--- docs/conf.py
+++ docs/conf.py
@@ -14,15 +14,33 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "myst_parser",
+    'sphinx_copybutton',
+    "sphinx_design",
+    'sphinxext.opengraph',
+    'sphinx_tabs.tabs',
+    'sphinx.ext.intersphinx',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
+source_suffix = '.md'
 
+master_doc = 'index'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
+
+# -- myst configuration -------------------------------------------------
+myst_enable_extensions = [
+    "colon_fence",
+    "linkify",
+    "replacements",
+]
+
+myst_heading_anchors = 4
